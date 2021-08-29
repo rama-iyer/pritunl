394b83bf33e0eef380f91ab8039fa812a1283849
rama-iyer <rama.p.iyer@gmail.com>
2021-08-29 07:26:18 -0400

    Create pritunl_install_centos7.sh

---
+++ pritunl_install_centos7.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+sudo tee /etc/yum.repos.d/mongodb-org-3.4.repo << EOF
+[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc
+EOF
+
+sudo tee /etc/yum.repos.d/pritunl.repo << EOF
+[pritunl]
+name=Pritunl Repository
+baseurl=https://repo.pritunl.com/stable/yum/centos/7/
+gpgcheck=1
+enabled=1
+EOF
+
+yum -y install epel-release
+sudo yum -y remove iptables-services
+sudo systemctl stop firewalld.service
+sudo systemctl disable firewalld.service
+
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+gpg --armor --export 7568D9BB55FF9E5287D586017AE645C0CF8E292A > key.tmp; sudo rpm --import key.tmp; rm -f key.tmp
+
+sudo yum -y install pritunl mongodb-org
+
+sudo systemctl enable mongod pritunl
+sudo systemctl start mongod pritunl
+
+
+sudo lsof -p `pgrep pritunl-web` | wc -l
+
+sudo sh -c 'echo "* hard nofile 64000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "* soft nofile 64000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "root hard nofile 64000" >> /etc/security/limits.conf'
+sudo sh -c 'echo "root soft nofile 64000" >> /etc/security/limits.conf'
+
+sudo pritunl setup-key
